955de5d7978991d741ae3f7cf1946cc003cb7956
Michael Raymond <michael_raymond@fastmail.com>
2022-08-12 22:41:26 +0100

    Fix blocking intermediate chars

--- src/normalise.rs
+++ src/normalise.rs
@@ -44,7 +44,7 @@ fn decompose(cp: u32) -> Vec<u32> {
 }
 
 pub fn to_nfc_str(bytes: Vec<u8>) -> Vec<u8> {
-    // TODO: this intermediate vec isn't necessary, I've just split up the funcs
+    // Note: this intermediate vec isn't necessary, I've just split up the funcs
     // because the provided test cases are in code points, not utf-8
     let cps: Vec<u32> = CodePointIter::new(bytes).collect();
     to_nfc(&cps).into_iter().flat_map(encode_utf8).collect()
@@ -62,19 +62,25 @@ fn to_nfc(cps: &Vec<u32>) -> Vec<u32> {
     loop {
         if try_compose {
             try_compose = false;
-            // TODO: this fails one of the cases, because the pairs aren't just Starter..next-starter.
-            // If there are multiple combining marks in a row that have the same ccc, then that will block it.
-            // e.g. 0x61 0x5ae 0x0300 is ok, the first and third compose, but 0x61 0x5ae 0x305 0x0300 won't
-            // because the 305 blocks the 300
             let next_starter_offset = decomposed[pos..].iter().skip(1).position(|cp| is_starter(*cp)).map(|offset| offset + 1).unwrap_or(decomposed.len() - pos);
             let char_seq_end = min(next_starter_offset + 1, decomposed.len() - pos);
             decomposed[pos..(pos + next_starter_offset)].sort_by(|a, b| combining_class(*a).cmp(&combining_class(*b)));
+
+            let mut last_ccc = 0;
             for i in 1..char_seq_end {
+                let ccc = combining_class(decomposed[pos + i]);
                 if let Some(composite) = primary_composite(decomposed[pos], decomposed[pos + i]) {
+                    // A starter-combining mark pair may be blocked by an intervening combining mark
+                    // if it has the same ccc.
+                    if ccc > 0 && ccc == last_ccc { break; }
+                    // A starter-starter pair is blocked if there are combining marks in between.
+                    if ccc < last_ccc { break; }
                     decomposed[pos] = composite;
                     decomposed.remove(pos + i);
                     try_compose = true;
                     break;
+                } else {
+                    last_ccc = ccc;
                 }
             }
         } else {
@@ -113,7 +119,7 @@ mod tests {
     #      c2 ==  toNFC(c1) ==  toNFC(c2) ==  toNFC(c3)
     #      c4 ==  toNFC(c4) ==  toNFC(c5)
     */
-    fn conformance_test(c: Vec<Vec<u32>>) {
+    fn nfc_conformance_test(c: Vec<Vec<u32>>) {
         assert_eq!(c[1], to_nfc(&c[0]));
         assert_eq!(c[1], to_nfc(&c[1]));
         assert_eq!(c[1], to_nfc(&c[2]));
@@ -127,94 +133,16 @@ mod tests {
 
     #[test]
     fn test_to_nfc() {
+        // https://www.unicode.org/Public/14.0.0/ucd/NormalizationTest.txt
         let test_cases: Vec<Vec<Vec<u32>>> = std::fs::read_to_string(std::path::Path::new("resources/NormalizationTest.txt"))
             .unwrap()
             .split("\n")
-            .filter(|line| !line.starts_with("#") && !line.starts_with("@"))
+            .filter(|line| !line.is_empty() && !line.starts_with("#") && !line.starts_with("@"))
             .map(parse_line)
             .collect();
 
-        // conformance_test(vec![vec![0x00A0], vec![0x00A0],vec![0x00A0],vec![0x0020],vec![0x0020]]);
         for case in test_cases {
-            conformance_test(case);
+            nfc_conformance_test(case);
         }
-
-        //
-        //
-        // // https://www.unicode.org/Public/14.0.0/ucd/NormalizationTest.txt
-        // // 1E0A;1E0A;0044 0307;1E0A;0044 0307; # (Ḋ; Ḋ; D◌̇; Ḋ; D◌̇; ) LATIN CAPITAL LETTER D WITH DOT ABOVE
-        // conformance_test(vec![vec![0x1E0A], vec![0x1E0A], vec![0x0044, 0x0307], vec![0x1E0A], vec![0x0044, 0x0307]]);
-        //
-        // // 1E0C;1E0C;0044 0323;1E0C;0044 0323; # (Ḍ; Ḍ; D◌̣; Ḍ; D◌̣; ) LATIN CAPITAL LETTER D WITH DOT BELOW
-        // conformance_test(vec![vec![0x1E0C], vec![0x1E0C], vec![0x0044, 0x0323], vec![0x1E0C], vec![0x0044, 0x0323]]);
-        //
-        // // 1E0A 0323;1E0C 0307;0044 0323 0307;1E0C 0307;0044 0323 0307; # (Ḋ◌̣; Ḍ◌̇; D◌̣◌̇; Ḍ◌̇; D◌̣◌̇; ) LATIN CAPITAL LETTER D WITH DOT ABOVE, COMBINING DOT BELOW
-        // conformance_test(vec![vec![0x1E0A, 0x0323], vec![0x1E0C, 0x0307], vec![0x0044, 0x0323, 0x0307], vec![0x1E0C, 0x0307], vec![0x0044, 0x0323, 0x0307]]);
-        //
-        // // 1E0C 0307;1E0C 0307;0044 0323 0307;1E0C 0307;0044 0323 0307; # (Ḍ◌̇; Ḍ◌̇; D◌̣◌̇; Ḍ◌̇; D◌̣◌̇; ) LATIN CAPITAL LETTER D WITH DOT BELOW, COMBINING DOT ABOVE
-        // conformance_test(vec![vec![0x1E0C, 0x0307], vec![0x1E0C, 0x0307], vec![0x0044, 0x0323, 0x0307], vec![0x1E0C, 0x0307], vec![0x0044, 0x0323, 0x0307]]);
-        //
-        // // 0044 0307 0323;1E0C 0307;0044 0323 0307;1E0C 0307;0044 0323 0307; # (D◌̇◌̣; Ḍ◌̇; D◌̣◌̇; Ḍ◌̇; D◌̣◌̇; ) LATIN CAPITAL LETTER D, COMBINING DOT ABOVE, COMBINING DOT BELOW
-        // conformance_test(vec![vec![0x0044, 0x0307, 0x0323], vec![0x1E0C, 0x0307], vec![0x0044, 0x0323, 0x0307], vec![0x1E0C, 0x0307], vec![0x0044, 0x0323, 0x0307]]);
-        //
-        // // 0044 0323 0307;1E0C 0307;0044 0323 0307;1E0C 0307;0044 0323 0307; # (D◌̣◌̇; Ḍ◌̇; D◌̣◌̇; Ḍ◌̇; D◌̣◌̇; ) LATIN CAPITAL LETTER D, COMBINING DOT BELOW, COMBINING DOT ABOVE
-        // conformance_test(vec![vec![0x0044, 0x0323, 0x0307], vec![0x1E0C, 0x0307], vec![0x0044, 0x0323, 0x0307], vec![0x1E0C, 0x0307], vec![0x0044, 0x0323, 0x0307]]);
-        //
-        // // 1E0A 031B;1E0A 031B;0044 031B 0307;1E0A 031B;0044 031B 0307; # (Ḋ◌̛; Ḋ◌̛; D◌̛◌̇; Ḋ◌̛; D◌̛◌̇; ) LATIN CAPITAL LETTER D WITH DOT ABOVE, COMBINING HORN
-        // conformance_test(vec![vec![0x1E0A, 0x031B], vec![0x1E0A, 0x031B], vec![0x0044, 0x031B, 0x0307], vec![0x1E0A, 0x031B], vec![0x0044, 0x031B, 0x0307]]);
-        //
-        // // 1E0C 031B;1E0C 031B;0044 031B 0323;1E0C 031B;0044 031B 0323; # (Ḍ◌̛; Ḍ◌̛; D◌̛◌̣; Ḍ◌̛; D◌̛◌̣; ) LATIN CAPITAL LETTER D WITH DOT BELOW, COMBINING HORN
-        // conformance_test(vec![vec![0x1E0C, 0x031B], vec![0x1E0C, 0x031B], vec![0x0044, 0x031B, 0x0323], vec![0x1E0C, 0x031B], vec![0x0044, 0x031B, 0x0323]]);
-        //
-        // // 1E0A 031B 0323;1E0C 031B 0307;0044 031B 0323 0307;1E0C 031B 0307;0044 031B 0323 0307; # (Ḋ◌̛◌̣; Ḍ◌̛◌̇; D◌̛◌̣◌̇; Ḍ◌̛◌̇; D◌̛◌̣◌̇; ) LATIN CAPITAL LETTER D WITH DOT ABOVE, COMBINING HORN, COMBINING DOT BELOW
-        // conformance_test(vec![vec![0x1E0A, 0x031B, 0x0323], vec![0x1E0C, 0x031B, 0x0307], vec![0x0044, 0x031B, 0x0323, 0x0307], vec![0x1E0C, 0x031B, 0x0307], vec![0x0044, 0x031B, 0x0323, 0x0307]]);
-        //
-        // // 1E0C 031B 0307;1E0C 031B 0307;0044 031B 0323 0307;1E0C 031B 0307;0044 031B 0323 0307; # (Ḍ◌̛◌̇; Ḍ◌̛◌̇; D◌̛◌̣◌̇; Ḍ◌̛◌̇; D◌̛◌̣◌̇; ) LATIN CAPITAL LETTER D WITH DOT BELOW, COMBINING HORN, COMBINING DOT ABOVE
-        // conformance_test(vec![vec![0x1E0C, 0x031B, 0x0307], vec![0x1E0C, 0x031B, 0x0307], vec![0x0044, 0x031B, 0x0323, 0x0307], vec![0x1E0C, 0x031B, 0x0307], vec![0x0044, 0x031B, 0x0323, 0x0307]]);
-        //
-        // // 0044 031B 0307 0323;1E0C 031B 0307;0044 031B 0323 0307;1E0C 031B 0307;0044 031B 0323 0307; # (D◌̛◌̇◌̣; Ḍ◌̛◌̇; D◌̛◌̣◌̇; Ḍ◌̛◌̇; D◌̛◌̣◌̇; ) LATIN CAPITAL LETTER D, COMBINING HORN, COMBINING DOT ABOVE, COMBINING DOT BELOW
-        // conformance_test(vec![vec![0x0044, 0x031B, 0x0307, 0x0323], vec![0x1E0C, 0x031B, 0x0307], vec![0x0044, 0x031B, 0x0323, 0x0307], vec![0x1E0C, 0x031B, 0x0307], vec![0x0044, 0x031B, 0x0323, 0x0307]]);
-        //
-        // // 0044 031B 0323 0307;1E0C 031B 0307;0044 031B 0323 0307;1E0C 031B 0307;0044 031B 0323 0307; # (D◌̛◌̣◌̇; Ḍ◌̛◌̇; D◌̛◌̣◌̇; Ḍ◌̛◌̇; D◌̛◌̣◌̇; ) LATIN CAPITAL LETTER D, COMBINING HORN, COMBINING DOT BELOW, COMBINING DOT ABOVE
-        // conformance_test(vec![vec![0x0044, 0x031B, 0x0323, 0x0307], vec![0x1E0C, 0x031B, 0x0307], vec![0x0044, 0x031B, 0x0323, 0x0307], vec![0x1E0C, 0x031B, 0x0307], vec![0x0044, 0x031B, 0x0323, 0x0307]]);
-        //
-        // // 00C8;00C8;0045 0300;00C8;0045 0300; # (È; È; E◌̀; È; E◌̀; ) LATIN CAPITAL LETTER E WITH GRAVE
-        // conformance_test(vec![vec![0x00C8], vec![0x00C8], vec![0x0045, 0x0300], vec![0x00C8], vec![0x0045, 0x0300]]);
-        //
-        // // 0112;0112;0045 0304;0112;0045 0304; # (Ē; Ē; E◌̄; Ē; E◌̄; ) LATIN CAPITAL LETTER E WITH MACRON
-        // conformance_test(vec![vec![0x0112], vec![0x0112], vec![0x0045, 0x0304], vec![0x0112], vec![0x0045, 0x0304]]);
-        //
-        // // 0045 0300;00C8;0045 0300;00C8;0045 0300; # (E◌̀; È; E◌̀; È; E◌̀; ) LATIN CAPITAL LETTER E, COMBINING GRAVE ACCENT
-        // conformance_test(vec![vec![0x0045, 0x0300], vec![0x00C8], vec![0x0045, 0x0300], vec![0x00C8], vec![0x0045, 0x0300]]);
-        //
-        // // 0045 0304;0112;0045 0304;0112;0045 0304; # (E◌̄; Ē; E◌̄; Ē; E◌̄; ) LATIN CAPITAL LETTER E, COMBINING MACRON
-        // conformance_test(vec![vec![0x0045, 0x0304], vec![0x0112], vec![0x0045, 0x0304], vec![0x0112], vec![0x0045, 0x0304]]);
-        //
-        // // 1E14;1E14;0045 0304 0300;1E14;0045 0304 0300; # (Ḕ; Ḕ; E◌̄◌̀; Ḕ; E◌̄◌̀; ) LATIN CAPITAL LETTER E WITH MACRON AND GRAVE
-        // conformance_test(vec![vec![0x1E14], vec![0x1E14], vec![0x0045, 0x0304, 0x0300], vec![0x1E14], vec![0x0045, 0x0304, 0x0300]]);
-        //
-        // // 0112 0300;1E14;0045 0304 0300;1E14;0045 0304 0300; # (Ē◌̀; Ḕ; E◌̄◌̀; Ḕ; E◌̄◌̀; ) LATIN CAPITAL LETTER E WITH MACRON, COMBINING GRAVE ACCENT
-        // conformance_test(vec![vec![0x0112, 0x0300], vec![0x1E14], vec![0x0045, 0x0304, 0x0300], vec![0x1E14], vec![0x0045, 0x0304, 0x0300]]);
-        //
-        // // 1E14 0304;1E14 0304;0045 0304 0300 0304;1E14 0304;0045 0304 0300 0304; # (Ḕ◌̄; Ḕ◌̄; E◌̄◌̀◌̄; Ḕ◌̄; E◌̄◌̀◌̄; ) LATIN CAPITAL LETTER E WITH MACRON AND GRAVE, COMBINING MACRON
-        // conformance_test(vec![vec![0x1E14, 0x0304], vec![0x1E14, 0x0304], vec![0x0045, 0x0304, 0x0300, 0x0304], vec![0x1E14, 0x0304], vec![0x0045, 0x0304, 0x0300, 0x0304]]);
-        //
-        // // 0045 0304 0300;1E14;0045 0304 0300;1E14;0045 0304 0300; # (E◌̄◌̀; Ḕ; E◌̄◌̀; Ḕ; E◌̄◌̀; ) LATIN CAPITAL LETTER E, COMBINING MACRON, COMBINING GRAVE ACCENT
-        // conformance_test(vec![vec![0x0045, 0x0304, 0x0300], vec![0x1E14], vec![0x0045, 0x0304, 0x0300], vec![0x1E14], vec![0x0045, 0x0304, 0x0300]]);
-        //
-        // // 0045 0300 0304;00C8 0304;0045 0300 0304;00C8 0304;0045 0300 0304; # (E◌̀◌̄; È◌̄; E◌̀◌̄; È◌̄; E◌̀◌̄; ) LATIN CAPITAL LETTER E, COMBINING GRAVE ACCENT, COMBINING MACRON
-        // conformance_test(vec![vec![0x0045, 0x0300, 0x0304], vec![0x00C8, 0x0304], vec![0x0045, 0x0300, 0x0304], vec![0x00C8, 0x0304], vec![0x0045, 0x0300, 0x0304]]);
-        //
-        // // 05B8 05B9 05B1 0591 05C3 05B0 05AC 059F;05B1 05B8 05B9 0591 05C3 05B0 05AC 059F;05B1 05B8 05B9 0591 05C3 05B0 05AC 059F;05B1 05B8 05B9 0591 05C3 05B0 05AC 059F;05B1 05B8 05B9 0591 05C3 05B0 05AC 059F; # (◌ָ◌ֹ◌ֱ◌֑׃◌ְ◌֬◌֟; ◌ֱ◌ָ◌ֹ◌֑׃◌ְ◌֬◌֟; ◌ֱ◌ָ◌ֹ◌֑׃◌ְ◌֬◌֟; ◌ֱ◌ָ◌ֹ◌֑׃◌ְ◌֬◌֟; ◌ֱ◌ָ◌ֹ◌֑׃◌ְ◌֬◌֟; ) HEBREW POINT QAMATS, HEBREW POINT HOLAM, HEBREW POINT HATAF SEGOL, HEBREW ACCENT ETNAHTA, HEBREW PUNCTUATION SOF PASUQ, HEBREW POINT SHEVA, HEBREW ACCENT ILUY, HEBREW ACCENT QARNEY PARA
-        // conformance_test(vec![vec![0x05B8, 0x05B9, 0x05B1, 0x0591, 0x05C3, 0x05B0, 0x05AC, 0x059F], vec![0x05B1, 0x05B8, 0x05B9, 0x0591, 0x05C3, 0x05B0, 0x05AC, 0x059F], vec![0x05B1, 0x05B8, 0x05B9, 0x0591, 0x05C3, 0x05B0, 0x05AC, 0x059F], vec![0x05B1, 0x05B8, 0x05B9, 0x0591, 0x05C3, 0x05B0, 0x05AC, 0x059F], vec![0x05B1, 0x05B8, 0x05B9, 0x0591, 0x05C3, 0x05B0, 0x05AC, 0x059F]]);
-        //
-        // // 0592 05B7 05BC 05A5 05B0 05C0 05C4 05AD;05B0 05B7 05BC 05A5 0592 05C0 05AD 05C4;05B0 05B7 05BC 05A5 0592 05C0 05AD 05C4;05B0 05B7 05BC 05A5 0592 05C0 05AD 05C4;05B0 05B7 05BC 05A5 0592 05C0 05AD 05C4; # (◌֒◌ַ◌ּ◌֥◌ְ׀◌ׄ◌֭; ◌ְ◌ַ◌ּ◌֥◌֒׀◌֭◌ׄ; ◌ְ◌ַ◌ּ◌֥◌֒׀◌֭◌ׄ; ◌ְ◌ַ◌ּ◌֥◌֒׀◌֭◌ׄ; ◌ְ◌ַ◌ּ◌֥◌֒׀◌֭◌ׄ; ) HEBREW ACCENT SEGOL, HEBREW POINT PATAH, HEBREW POINT DAGESH OR MAPIQ, HEBREW ACCENT MERKHA, HEBREW POINT SHEVA, HEBREW PUNCTUATION PASEQ, HEBREW MARK UPPER DOT, HEBREW ACCENT DEHI
-        // conformance_test(vec![vec![0x0592, 0x05B7, 0x05BC, 0x05A5, 0x05B0, 0x05C0, 0x05C4, 0x05AD], vec![0x05B0, 0x05B7, 0x05BC, 0x05A5, 0x0592, 0x05C0, 0x05AD, 0x05C4], vec![0x05B0, 0x05B7, 0x05BC, 0x05A5, 0x0592, 0x05C0, 0x05AD, 0x05C4], vec![0x05B0, 0x05B7, 0x05BC, 0x05A5, 0x0592, 0x05C0, 0x05AD, 0x05C4], vec![0x05B0, 0x05B7, 0x05BC, 0x05A5, 0x0592, 0x05C0, 0x05AD, 0x05C4]]);
-        //
-        // // 1100 AC00 11A8;1100 AC01;1100 1100 1161 11A8;1100 AC01;1100 1100 1161 11A8; # (ᄀ각; ᄀ각; ᄀ각; ᄀ각; ᄀ각; ) HANGUL CHOSEONG KIYEOK, HANGUL SYLLABLE GA, HANGUL JONGSEONG KIYEOK
-        // conformance_test(vec![vec![0x1100, 0xAC00, 0x11A8], vec![0x1100, 0xAC01], vec![0x1100, 0x1100, 0x1161, 0x11A8], vec![0x1100, 0xAC01], vec![0x1100, 0x1100, 0x1161, 0x11A8]]);
-        //
-        // // 1100 AC00 11A8 11A8;1100 AC01 11A8;1100 1100 1161 11A8 11A8;1100 AC01 11A8;1100 1100 1161 11A8 11A8; # (ᄀ각ᆨ; ᄀ각ᆨ; ᄀ각ᆨ; ᄀ각ᆨ; ᄀ각ᆨ; ) HANGUL CHOSEONG KIYEOK, HANGUL SYLLABLE GA, HANGUL JONGSEONG KIYEOK, HANGUL JONGSEONG KIYEOK
-        // conformance_test(vec![vec![0x1100, 0xAC00, 0x11A8, 0x11A8], vec![0x1100, 0xAC01, 0x11A8], vec![0x1100, 0x1100, 0x1161, 0x11A8, 0x11A8], vec![0x1100, 0xAC01, 0x11A8], vec![0x1100, 0x1100, 0x1161, 0x11A8, 0x11A8]]);
     }
 }
